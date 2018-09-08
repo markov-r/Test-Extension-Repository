f0e7501f215deb4c8ca1dbfdfefce940a94658eb
Rado <markov.r@abv.bg>
2018-09-08 15:15:04 +0300

    Clearing old stuff.

--- src/main/java/com/telerik/extension_repository/services/ExtensionServiceImpl.java
+++ src/main/java/com/telerik/extension_repository/services/ExtensionServiceImpl.java
@@ -176,12 +176,6 @@ public class ExtensionServiceImpl implements ExtensionService {
         HashSet<Tag> tags = getTags(addExtensionModel);
         extension.setTags(tags);
         extension.setUploadDate(new Date());
-//         //MvcUriComponentsBuilder prepares the URL based on the method which is going to actually serve the file for download
-//        String downloadLink =  MvcUriComponentsBuilder.fromMethodName(ExtensionController.class,
-//                "serveFile", addExtensionModel.getMultipartFile().getOriginalFilename()).build().toString();
-//        extension.setDownloadLink(downloadLink);
-        //System.out.println(addExtensionModel.getFile().getName());
-//        System.out.print(addExtensionModel.getFile().getOriginalFilename());
         extension.setFileName(addExtensionModel.getFile().getOriginalFilename());
         this.extensionRepository.saveAndFlush(extension);
     }
