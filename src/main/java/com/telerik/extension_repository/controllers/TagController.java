54294ce55ef969722f9ef06ce6c6a987f8a6cdf9
Rado <markov.r@abv.bg>
2018-09-08 16:09:04 +0300

    Clearing compiler warnings 05

--- src/main/java/com/telerik/extension_repository/controllers/TagController.java
+++ src/main/java/com/telerik/extension_repository/controllers/TagController.java
@@ -10,20 +10,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TagController {
+
     @Autowired
     private TagRepository tagRepository;
 
     @GetMapping("/tag/{name}")
     public String extensionsWithTag(Model model, @PathVariable String name){
         Tag tag=this.tagRepository.findByName(name);
-
         if(tag == null){
             return "redirect:/extensions/all";
         }
-
         model.addAttribute("view", "/tags/extensions");
         model.addAttribute("tag", tag);
-
         return "base-layout";
     }
 }
