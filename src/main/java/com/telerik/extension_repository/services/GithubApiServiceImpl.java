f840bba32ac3f0bdb655115fbfd583b6e834f043
Rado <markov.r@abv.bg>
2018-09-08 10:38:48 +0300

    Username exists validator fix.

--- src/main/java/com/telerik/extension_repository/services/GithubApiServiceImpl.java
+++ src/main/java/com/telerik/extension_repository/services/GithubApiServiceImpl.java
@@ -77,7 +77,7 @@ public class GithubApiServiceImpl implements GithubApiService/*, InitializingBea
     @PostConstruct
     private void triggerGitUpdate() {
 
-        Thread gitThread = new Thread(() -> {   //TODO: replace anonymous class creation with standard class extending Thread.
+        Thread gitThread = new Thread(() -> {   //TODO: replace anonymous class creation with standard class that extends Thread.
             System.out.println("#######" + Thread.currentThread().getName() + "########");
             while (true) {
                 try {
