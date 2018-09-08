5c562c2a762c673ced22922cab91eb5f2fa581b0
Rado <markov.r@abv.bg>
2018-09-08 11:08:01 +0300

    Thread no longer created as Anonymous class.

--- src/main/java/com/telerik/extension_repository/services/GithubApiServiceImpl.java
+++ src/main/java/com/telerik/extension_repository/services/GithubApiServiceImpl.java
@@ -1,7 +1,6 @@
 package com.telerik.extension_repository.services;
 
 import com.telerik.extension_repository.entities.GitHubData;
-import com.telerik.extension_repository.entities.Properties;
 import com.telerik.extension_repository.models.ExtensionDto;
 import com.telerik.extension_repository.models.PropertiesDto;
 import com.telerik.extension_repository.repositories.GitHubRepository;
@@ -13,16 +12,16 @@ import org.kohsuke.github.GitHub;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+
 import static com.telerik.extension_repository.utils.Constants.GITHUB_URL;
 import static com.telerik.extension_repository.utils.Constants.GIT_KEY;
 
 @Service
-public class GithubApiServiceImpl implements GithubApiService/*, InitializingBean */{
+public class GithubApiServiceImpl implements GithubApiService/*, InitializingBean */ {
 
     @Autowired
     private ExtensionService extensionService;
@@ -38,6 +37,18 @@ public class GithubApiServiceImpl implements GithubApiService/*, InitializingBea
 //        triggerGitUpdate();
 //    }
 
+    /**
+     * Start a new thread for running GitHub synchronization only.
+     * As the thread is forked from main, it should shut down together with
+     * main thread, so no need for setting it as daemon.
+     */
+    @PostConstruct
+    private void triggerGitUpdate() {
+        GitUpdateThread gitThread = new GitUpdateThread();
+        gitThread.start();
+//        gitThread.setDaemon(true);
+    }
+
     @Override
     public GitHub getGHConnection() throws IOException {
         return GitHub.connectUsingOAuth(GIT_KEY);
@@ -47,7 +58,7 @@ public class GithubApiServiceImpl implements GithubApiService/*, InitializingBea
         List<ExtensionDto> extensionDtoList = this.extensionService.getAllExt();
         for (ExtensionDto extensionDto : extensionDtoList) {
             String fullUrl = extensionDto.getSource_repository_link();
-             try {
+            try {
                 GitHubData gitHubData = updateGithubData(fullUrl);
                 extensionDto.setGitHubData(gitHubData);
                 String pullsCount = gitHubData.getPullsCount();
@@ -56,48 +67,12 @@ public class GithubApiServiceImpl implements GithubApiService/*, InitializingBea
 //                String lastCommit = lastCommitDate.toString();
                 Long id = extensionDto.getId();
                 this.gitHubRepository.update(pullsCount, issuesCount, lastCommitDate, id);
-                 System.out.println("### GH DATA UPDATED ### " + extensionDto.getName());
+                System.out.println("### GH DATA UPDATED ### " + extensionDto.getName());
             } catch (Exception e) {
-                 System.out.println(e.getMessage());
+                System.out.println(e.getMessage());
                 e.getMessage();
             }
         }
-    }
-
-//    public class MyThread extends Thread {
-//        public void run(){
-//            System.out.println("MyThread running");
-//        }
-//    }
-
-/** Start a new thread for running GitHub synchronization only.
-    Thread is set to daemon so that when main thread ends
-    this should not stop JVM from shutting down. */
-
-    @PostConstruct
-    private void triggerGitUpdate() {
-
-        Thread gitThread = new Thread(() -> {   //TODO: replace anonymous class creation with standard class that extends Thread.
-            System.out.println("#######" + Thread.currentThread().getName() + "########");
-            while (true) {
-                try {
-                    updateGithubDataAll();
-                    propertiesService.updateLastSuccSync(new Date());
-                    PropertiesDto prop = this.propertiesService.getProperties();
-                    long interval = prop.getUpdateInterval();
-                    System.out.println("INTERVAL IS -> " + interval);
-                    Thread.sleep(interval);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread is interrupted!");
-                    e.getMessage();
-                    propertiesService.updateFailureDetails("Git Data update failed due to an interrupted thread.");
-                    propertiesService.updateLastFailedSync(new Date());
-                    Thread.currentThread().interrupt();
-                }
-            }
-        });
-//        gitThread.setDaemon(true);
-        gitThread.start();
     }
 
     @Override
@@ -126,6 +101,27 @@ public class GithubApiServiceImpl implements GithubApiService/*, InitializingBea
     public String getPullsCount(String url) throws IOException {
         GitHub git = getGHConnection();
         return Integer.toString(git.getRepository(url).getPullRequests(GHIssueState.ALL).size());
+    }
+
+    public class GitUpdateThread extends Thread {
+        public void run() {
+            while (true) {
+                try {
+                    updateGithubDataAll();
+                    propertiesService.updateLastSuccSync(new Date());
+                    PropertiesDto prop = propertiesService.getProperties();
+                    long interval = prop.getUpdateInterval();
+                    System.out.println("INTERVAL IS -> " + interval);
+                    Thread.sleep(interval);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread is interrupted!");
+                    e.getMessage();
+                    propertiesService.updateFailureDetails("Git Data update failed due to an interrupted thread.");
+                    propertiesService.updateLastFailedSync(new Date());
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 
 }
